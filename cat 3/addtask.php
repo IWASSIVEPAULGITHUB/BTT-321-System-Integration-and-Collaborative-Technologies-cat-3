e6b4e85090ecdee90bd49163e883163d8994e9c7
Paul K <150119085+IWASSIVEPAULGITHUB@users.noreply.github.com>
2025-07-24 11:52:43 +0300

    Delete cat 3 directory

--- cat 3/addtask.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'db.php';
-$title = $_POST['title'];
-$stmt = $conn->prepare("INSERT INTO tasks (title, status) VALUES (?, 'To Do')");
-$stmt->bind_param("s", $title);
-$stmt->execute();
-header("Location: index1.php");
